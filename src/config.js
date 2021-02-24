7c24c6147bea0d03ed73e02ad537b9bf4fb33b8a
Ming Cheng <cmgrace@bu.edu>
2021-02-24 11:49:43 -0500

    revise

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // const API_ENDPOINT = "http://localhost:8000/api";
 // export default API_ENDPOINT;
-export default {
-  REACT_APP_API_BASE:
-    process.env.REACT_APP_API_BASE ||
-    "https://warm-reaches-49846.herokuapp.com/api",
-};
+
+const REACT_APP_API_BASE =
+  process.env.REACT_APP_API_BASE ||
+  "https://warm-reaches-49846.herokuapp.com/api";
+export default REACT_APP_API_BASE;
